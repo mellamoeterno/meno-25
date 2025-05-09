764b16f864f06abb1f6d310413b0e66f268414d1
mellamoeterno <richmyself8@gmail.com>
2025-05-09 03:36:41 -0300

    changedPlaceHolderKey

--- src/app/collect/page.js
+++ src/app/collect/page.js
@@ -30,7 +30,7 @@ export default function CollectEmailPage() {
         <h1 className="text-2xl font-bold text-gray-800">Receba a sua amostra do E-book grátis pelo seu E-mail </h1>
         <input
           type="email"
-          placeholder="you@example.com"
+          placeholder="você@porexemplo.com"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           className="w-full px-4 py-2 border border-blue-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
